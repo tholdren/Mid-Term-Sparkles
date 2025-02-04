179efdb0e67eb2f517970b524b7d370c2d7e5a78
Tristan Holdren <tristan.holdren@gmail.com>
2025-02-04 18:24:10 +0000

    Imported Cod

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("👍 Okay");
+// ✅ Add External ColorKit Library
+// Color Kit Library
+// https://codepen.io/manikoth/pen/NPKVOxm.js
+
+// Get Stuff
+
+// Set variable for HTML `body` element
+
+const body = document.querySelector("body");
+
+// ✅ Check-Check: log out the `body` element in the console
+
+console.log(body);
+
+// ✅ Check-Check: get and attach the node for the background color with `.style.backgroundColor`
+
+// Set Stuff
+
+// ✅ Check-Check: assign a background color to "hard code it"
+// Hint: Is the color a string, number, or boolean?
+
+body.style.backgroundColor = "red";
+
+// dynamically set backgound color with `colorkit.random()` function/method
+
+body.style.backgroundColor = colorkit.random();
+
+// Sparkle effect for funzies
+// replace❓ with variable for the `body` element
+// colorkit.sparkle( ❓);
+
+colorkit.sparkle(body);
